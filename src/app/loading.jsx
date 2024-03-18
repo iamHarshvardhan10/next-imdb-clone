0280bed80a3d44a7556bb3942518839fdb04e275
Harshvardhan <shreeharshpb11@gmail.com>
2024-03-19 01:13:40 +0530

    fixes loading page bug

--- src/app/loading.jsx
+++ src/app/loading.jsx
@@ -1,9 +1,10 @@
+import Image from 'next/image';
 import React from 'react'
 
 const Loading = () => {
   return (
     <div className='flex justify-center'>
-        <img src="spinner1.svg" alt="" />
+        <Image src="spinner1.svg" alt="" />
     </div>
   )
 }
