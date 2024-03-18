e206eb4371ddb391e61af0d6cca18364d9f5e7e2
Harshvardhan <shreeharshpb11@gmail.com>
2024-03-18 21:21:52 +0530

    handling error

---
+++ src/app/error.jsx
@@ -0,0 +1,18 @@
+'use client'
+
+import React from 'react'
+import { useEffect } from 'react'
+
+const error = ({error , reset}) => {
+    useEffect(() => {
+        console.log(error)
+    },[error])
+  return (
+    <div>
+        <h1>Something went Wrong</h1>
+        <button onClick={() => reset()}>Try Again</button>
+    </div>
+  )
+}
+
+export default error
