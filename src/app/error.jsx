e26dea608843a551b3cae910c16f0ef5a2395e32
Harshvardhan <shreeharshpb11@gmail.com>
2024-03-19 01:11:42 +0530

    fixes bugs

--- src/app/error.jsx
+++ src/app/error.jsx
@@ -3,14 +3,15 @@
 import React from 'react'
 import { useEffect } from 'react'
 
-const Error = ({error , reset}) => {
-    useEffect(() => {
-        console.log(error)
-    },[error])
+const Error = ({ error, reset }) => {
+  useEffect(() => {
+    console.log(error)
+  }, [error])
+
   return (
     <div>
-        <h1>Something went Wrong</h1>
-        <button onClick={() => reset()}>Try Again</button>
+      <h1>Something went Wrong</h1>
+      <button onClick={() => reset()}>Try Again</button>
     </div>
   )
 }
