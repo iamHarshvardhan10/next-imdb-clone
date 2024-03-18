5cadecfacc3e7c86540b0c17338b4343e56301f6
Harshvardhan <shreeharshpb11@gmail.com>
2024-03-18 12:08:31 +0530

    create about page

---
+++ src/app/about/page.jsx
@@ -0,0 +1,45 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="max-w-6xl mx-auto p-3 space-y-4">
+      <h1 className="text-2xl font-medium text-amber-600">About</h1>
+      <p>
+        Welcome to IMDb, your ultimate destination for everything related to
+        movies, TV shows, and entertainment.
+      </p>
+      <p>
+        At IMDb, we are passionate about providing you with the most
+        comprehensive and accurate information about your favorite films,
+        actors, directors, and more. Whether you are a casual moviegoer or a
+        dedicated cinephile, we have something for everyone.
+      </p>
+      <p>
+        Discover thousands of movies and TV shows, from timeless classics to the
+        latest blockbusters. Explore detailed information about each title,
+        including plot summaries, cast and crew details, trivia, quotes, and
+        user reviews. With our vast database, you can easily find
+        recommendations based on your personal preferences and interests.
+      </p>
+      <p>
+        Immerse yourself in the world of cinema with our extensive collection of
+        trailers, clips, and behind-the-scenes footage. Stay up-to-date with the
+        latest news, interviews, and industry updates from Hollywood and beyond.
+      </p>
+      <p>
+        Join our vibrant community of movie enthusiasts to share your thoughts,
+        ratings, and reviews. Connect with like-minded individuals, participate
+        in discussions, and engage in friendly debates about your favorite films
+        and TV shows.
+      </p>
+      <p>
+        Whether you are looking for entertainment inspiration or simply want to
+        dive deeper into the world of movies, IMDb is your go-to destination.
+        Start exploring today and unlock endless possibilities in the world of
+        cinema.
+      </p>
+    </div>
+  );
+};
+
+export default About;
